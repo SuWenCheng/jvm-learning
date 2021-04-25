36b7dfce51b8f08f3841b93fbfcbf4f9c87aa455
suwencheng <wencheng@imagedt.com>
2021-04-25 23:01:20 +0800

    新生代垃圾回收&老年代垃圾回收

--- src/main/java/com/al/jvm/learning/JvmLearningApplication.java
+++ src/main/java/com/al/jvm/learning/JvmLearningApplication.java
@@ -1,4 +1,4 @@
-package com.example.jvmlearning;
+package com.al.jvm.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/al/jvm/learning/JvmLearningApplicationTests.java
+++ src/test/java/com/al/jvm/learning/JvmLearningApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.jvmlearning;
+package com.al.jvm.learning;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
